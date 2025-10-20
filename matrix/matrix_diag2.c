a277a9b47106a086a93af4b2b2c048e8725128b2
antonio63963 <shotvideo25@gmail.com>
2025-10-20 17:31:59 +0300

    added: sum diag2

---
+++ matrix/matrix_diag2.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+#define ROWS 3
+#define COLUMNS 3
+
+int sum_diag2(int a[3][3]);
+
+
+int main()
+{
+  int a[ROWS][COLUMNS];
+  int sum = 0;
+
+  // чтение массива
+  for (int i = 0; i < ROWS; i++)
+  {
+    // чтение одной строки a[i]
+    for (int j = 0; j < COLUMNS; j++)
+    {
+      scanf("%d", &a[i][j]);
+    }
+  }
+
+  sum = sum_diag2(a);
+
+  printf("%d\n", sum);
+  return 0;
+}
+
+///===============
+int sum_diag2(int a[3][3])
+{
+  int diagSum = 0;
+  int colIdx = COLUMNS -1;
+  for (int i = 0; i < ROWS; i++)
+  {
+    diagSum += a[i][colIdx];
+    colIdx--;
+  }
+  return diagSum;
+}
