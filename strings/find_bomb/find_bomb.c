56b8fd3f5e5ab8828ae1c582f2b5bb033668b159
antonio63963 <shotvideo25@gmail.com>
2025-10-27 12:31:46 +0200

    find bomb

---
+++ strings/find_bomb/find_bomb.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+  char s[1000];
+  int isBomb = 0;
+
+  while(1==scanf("%1000s", s))  {
+    printf("%s\n", s);
+    if(strcmp(s, "bomb") == 0) {
+      isBomb = 1;
+      break;
+    }
+  }
+
+  printf("%s\n", isBomb ? "YES": "NO");
+  return 0;
+}
