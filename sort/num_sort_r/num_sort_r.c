9198e57fe7ccfc7bee80fbc3783da9c0e802d45b
antonio63963 <shotvideo25@gmail.com>
2025-11-01 15:31:29 +0200

    sort reverse

---
+++ sort/num_sort_r/num_sort_r.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int sortBA(const void * p1, const void * p2) {
+  int a = *(int*)p1;
+  int b = *(int *)p2;
+
+  if(a>b) {
+    return -1;
+  }else if(a<b) {
+    return 1;
+  }else {
+    return 0;
+  }
+} 
+
+int main() {
+  int n;
+  
+  scanf("%d", &n);
+  int arr[n];
+
+  for(int i = 0; i< n; i++) {
+    scanf("%d", &arr[i]);
+  }
+
+  qsort(arr, n, sizeof(arr[0]), sortBA);
+
+  for(int i = 0; i< n; i++) {
+    printf("%d ", arr[i]);
+  }
+
+  return 0;
+}
